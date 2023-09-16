075ebc9176c282474d84aa565c43eb705ee55ce5
Horvi <ivana.hrvtc@gmail.com>
2023-09-16 16:28:41 +0200

    Delete src directory

--- src/redux/actions/textInput.js
+++
@@ -1,6 +0,0 @@
-// create action creators to update the redux state
-
-export const updateTextInput = (text) => ({
-    type: 'UPDATE_TEXT_INPUT',
-    payload: text
-})
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-// create store
-
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./reducers";
-
-const store = createStore(rootReducer, applyMiddleware())
-
-export default store
